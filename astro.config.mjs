08e5f33c232480a394e54291f584772724552c10
tfzayed <zayed.themefisher@gmail.com>
2025-04-16 16:58:20 +0600

    updated to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import AutoImport from "astro-auto-import";
 import { defineConfig } from "astro/config";
 import remarkCollapse from "remark-collapse";
@@ -23,15 +23,14 @@ export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
+
+  vite: { plugins: [tailwindcss()] },
   image: { service: sharp() },
+
   integrations: [
     react(),
     sitemap(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
+
     AutoImport({
       imports: [
         "@/shortcodes/Button",
